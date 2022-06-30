041e029f7582a4b4c919a94ae8a071756682b49d
Przemysław Marszałek <p.marszalek@ideo.local>
2022-06-30 16:55:24 +0200

    some changes in scss

--- wp-content/themes/example-w1/header.php
+++ wp-content/themes/example-w1/header.php
@@ -27,7 +27,7 @@
 		<div class="main-container-px20 nav-container">
 			<div>
 				<a href="<?= get_home_url() ?>">
-					elCode.pl
+					elCode
 				</a>
 				<a href=""></a>
 				<a href=""></a>
@@ -62,7 +62,7 @@
 			<div class="nav-mobile">
 				<div class="nav-logo">
 					<a href="<?= get_home_url() ?>">
-						MarWeb.pl
+						Elcode
 					</a>
 					<div class="change-color-container">
 						<div class="color-icon color-icon-mobile">
